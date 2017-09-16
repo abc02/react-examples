e76372e6e9543f58b360b9d78c4a683aaa5533ed
abc02 <abcqq02@qq.com>
2017-09-16 23:40:06 +0800

    add README.md

--- app/index.js
+++ app/index.js
@@ -1,11 +1,15 @@
+import j from 'jquery'
 import _ from 'lodash'
 
 function component(){
-    var element = document.createElement('div')
+    // var element = document.createElement('div')
+    var element = j('<div></div>')
 
-    element.innerHTML = _.join(['Hello','Webpack'], ' ')
+    // element.innerHTML = _.join(['Hello','Webpack'], ' ')
+    element.html(_.join(['Hello','Webpack'], ' '))
 
-    return element
+    // return element
+    return element.get(0)
 }
 
 

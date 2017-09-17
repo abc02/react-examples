e60487c76587f8198f89e5d7258c9b991335a2b0
abc02 <abcqq02@qq.com>
2017-09-17 15:23:21 +0800

    edit package.json

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ function component(){
     var element = j('<div></div>')
 
     // element.innerHTML = _.join(['Hello','Webpack'], ' ')
-    element.html(_.join(['Hi','Webpack-dev-server'], ' '));
+    element.html(_.join(['Hi','webpack-dev-serve --open'], ' '));
 
     // return element
     return element.get(0)

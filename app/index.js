67a1c6d28cd3773ae9e5c362f2f04990461e131c
abc02 <abcqq02@qq.com>
2017-09-17 15:44:15 +0800

    edit webpack.config.js

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ function component(){
     var element = j('<div></div>')
 
     // element.innerHTML = _.join(['Hello','Webpack'], ' ')
-    element.html(_.join(['Hi','webpack-dev-serve --open'], ' '));
+    element.html(_.join(['Hi','不会热加载'], ' '));
 
     // return element
     return element.get(0)
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,10 @@ module.exports = {
 
     output:{
         filename:'bundle.js',
-        path: path.resolve(__dirname)
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer:{
+        contentBase:'./'
     }
 }
